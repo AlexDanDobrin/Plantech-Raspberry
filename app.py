cd49665e61889f1374aa36bec27ee8b31e68991f
AlexDanDobrin <alex.dan.dobrin@gmail.com>
2021-07-02 10:34:44 +0300

    This is the final commit

--- app.py
+++ app.py
@@ -16,22 +16,25 @@ relay_channel = int(os.environ.get('RELAY_CHANNEL'))
 user_id = int(os.environ.get('USER_ID'))
 sensor_id = int(os.environ.get('SENSOR_ID'))
 
+
 water_pump = WaterPump(relay_channel, 3)
 moisture_sensor = MoistureSensor()
 
+def convert_string_to_datetime(date: str) -> datetime:
+    pass
 
 def check_mode():
     # Verifica proprietatea auto a senzorului
     # Daca este setata pe auto atunci va efectua masuratori si irigatii automat
     # Altfel nu va masura si implicit nu va porni irigarea
-    res = requests.get(f'https://127.0.0.1:5000/getWorkMode/{sensor_id}')
+    res = requests.get(f'http://192.168.0.172:5000/getWorkMode/{sensor_id}')
     body = res.json()
     if body['mode'] == 'auto':
         return True
     return False
 
 def check_treshold():
-    res = requests.get(f'https://127.0.0.1:5000/getTreshold/{sensor_id}')
+    res = requests.get(f'http://192.168.0.172:5000/getTreshold/{sensor_id}')
     body = res.json()
     try:
         int(body['treshold'])
@@ -41,24 +44,29 @@ def check_treshold():
         return int(body['treshold'])
     
 def should_measure():
-    res = requests.get(f'https://127.0.0.1:5000/lastMeasurement/{sensor_id}')
+    res = requests.get(f'http://192.168.0.172:5000/lastMeasurement/{sensor_id}')
     data = res.json()
-    last_date = data['timestamp']
+    last_date = datetime.strptime(data["timestamp"][5:-4], "%d %b %Y %H:%M:%S")
     difference = datetime.now() - last_date
-    if difference.total_seconds > 3600:
+    if difference.seconds / 3600 >= 1:
+        print("Este necesara o masuratoare.")
         return True
+    print("Nu este necesara o masuratoare.")
     return False
 
 def register_measurement(value):
-    res = requests.post(f'https://127.0.0.1:5000/newMeasurement/{sensor_id}', data={'value': value})
+    print(f"S-a inregistrat masuratoare cu valoarea {value} pentru sensorul cu ID {sensor_id}.")
+    res = requests.post(f'http://192.168.0.172:5000/newMeasurement/{sensor_id}', data={'value': value})
 
     
 def DEMO():
-    res = requests.get('https://127.0.0.1:5000/startDEMO/')
-
+    res = requests.get('http://192.168.0.172:5000/startDEMO')
     if res.status_code == 200:
+        print("DEMO a fost activat si se initiaza procesul de irigare pentru trei secunde.")
         water_pump.start_watering()
-    
+    else:
+        print("Nu s-a activat functia DEMO.")
+
 
 
 while True:
@@ -74,7 +82,10 @@ while True:
             treshold = check_treshold()
             if isinstance(treshold, int):
                 if int(measurement) < treshold:
+                    print("Valoarea masurata este sub limita si se porneste irigarea pentru trei secunde.")
                     water_pump.start_watering()
+                else:
+                    print("Valoarea masurata este peste limita si nu este necesara irigarea.")
 
     # Verifica la fiecare 15 secunde daca s-a setat pe modul de lucru manual sau auto 
     sleep(15)
